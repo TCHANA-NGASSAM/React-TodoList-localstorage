bf9935e15bb1bf049e0bd73a7f8e73b54a4d384c
TCHANA-NGASSAM <ngassamadriano@gmail.com>
2023-11-04 12:07:18 +0100

    ajout de filtre et modifier

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,55 +1,78 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import '../style/todo.css'
 const TodoList = () => {
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
-  const [description, setDescription] = useState('');
-  const [dueDate, setDueDate] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
   const [filteredTasks, setFilteredTasks] = useState([]);
+  const [inputDate, setInputDate]= useState("")
+  const [currentIndex, setCurrentindex] = useState(0)
  // const [filterDate, setFilterDate] = useState("");
 
   useEffect(() => {
     const storedTasks = localStorage.getItem('tasks');
+    console.log(JSON.parse(storedTasks))
     if (storedTasks) {
       setTasks(JSON.parse(storedTasks));
+      setFilteredTasks(JSON.parse(storedTasks))
     }
+    console.log(filteredTasks)
   }, []);
 
-  useEffect(() => {
-    setFilteredTasks(
-      tasks.filter(task =>
-        task.description.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    );
-  }, [tasks, searchTerm]);
 
   const addTask = () => {
-    if (newTask !== '' && description !== '' && dueDate !== '') {
-      const task = {
-        id: Date.now(),
-        taskName: newTask,
-        description: description,
-        dueDate: dueDate
-      };
+    if (inputTodo.newTask !== '' && inputTodo.description !== '' && inputTodo.dueDate !== '') {
 
-      setTasks([...tasks, task]);
-      setNewTask('');
-      setDescription('');
-      setDueDate('');
+      setTasks([...tasks, inputTodo]);
+      setFilteredTasks([...tasks, inputTodo])
+      setinputTodo({description: '', dueDate: '', taskName:''});
 
       // Sauvegarde des tâches dans le localStorage
-      localStorage.setItem('tasks', JSON.stringify([...tasks, task]));
+      localStorage.setItem('tasks', JSON.stringify([...tasks, inputTodo]));
     }
   };
 
-  const deleteTask = id => {
-    const updatedTasks = tasks.filter(task => task.id !== id);
-    setTasks(updatedTasks);
-
-    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  const deleteTask = index => {
+    tasks.splice(index, 1)
+    setTasks(tasks);
+    setFilteredTasks(tasks);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   };
 
+
+  const handleInputChange = (e) => {
+    let array = tasks.filter(todo => todo.description.includes(e.target.value))
+    setFilteredTasks(array)
+  }
+  const handleDateChange = () => {
+    let datefilter = tasks.filter(todo => todo.dueDate.includes(inputDate))
+    setFilteredTasks(datefilter)
+  }
+  //const [inputTodo, setinputTodo] = useState("")
+  const [inputTodo, setinputTodo] = useState ({
+    taskName:"",
+    description: "",
+    dueDate: ""
+}) 
+
+const fillFormUpdate = (index) => {
+  setinputTodo(filteredTasks[index])
+  setCurrentindex(index)
+  setnewUpdate(true)
+}
+
+ const [newUpdate,setnewUpdate] = useState(false)
+
+ const updateTodo = (todo) => {
+    let array = tasks
+    array[currentIndex] = todo
+
+    setTasks(array)
+    setFilteredTasks(array)
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    
+    setinputTodo({description: '', dueDate: '', taskName:''});
+    setnewUpdate(false)
+ }
   // const datefilter   = tasks.filter((tasks) => {
   //   if (filterDate === "") {
   //     return true; // Afficher toutes les tâches si aucun filtre n'est appliqué
@@ -65,41 +88,54 @@ const TodoList = () => {
       <div>
       <input className='input'
           type="text"
-          value={description}
+          value={inputTodo.description}
           placeholder="Tâche"
-          onChange={e => setDescription(e.target.value)}
+          onChange={e => setinputTodo({...inputTodo, description:e.target.value} )}
         /> <br />
         <input className='input'
           type="text"
-          value={newTask}
+          value={inputTodo.taskName}
           placeholder=" Description"
-          onChange={e => setNewTask(e.target.value)}
+          onChange={e => setinputTodo({...inputTodo, taskName:e.target.value} )}
         /> <br />
        
         <input className='input'
           type="date"
-          value={dueDate}
-          onChange={e => setDueDate(e.target.value)}
+          value={inputTodo.dueDate}
+          onChange={e => setinputTodo({...inputTodo, dueDate:e.target.value} )}
         /> <br />
-        <button  className='btn-add' onClick={addTask}>AJOUTER VOTRE TACHE</button>
+        {
+          newUpdate ? (
+            <button  className='btn-ad' onClick={()=>{updateTodo(inputTodo)}}>MODIFIER VOTRE TACHE</button>
+          ) : (
+            <button  className='btn-add' onClick={addTask}>AJOUTER VOTRE TACHE</button>
+          )
+        }
+        
+      
+        <input type="date" 
+        className='inputDateFilter'
+        value={inputDate}
+        onChange={(e)=>setInputDate(e.target.value)}
+        />
+        <button className='btn-date' onClick={handleDateChange}>filtre par date</button>
       </div>
 
       <div>
         <input className='input'
           type="text"
           placeholder="Recherche"
-          onChange={e => setSearchTerm(e.target.value)}
+          onChange={handleInputChange}
         />
       </div>
 
       <ul className='list'>
-        {filteredTasks.map(task => (
+        {filteredTasks.map((task, index) => (
           <li className='li' key={task.id}> 
             <div className='flex'>
               <strong> {task.description}</strong>/{task.taskName}/{task.dueDate}
-              <button className='btn-sup' onClick={() => deleteTask(task.id)}>Supprimer</button>
-              
-            
+              <button className='btn-sup' onClick={() => deleteTask(index)}>Supprimer</button>
+              <button className='btn-modif' onClick={()=>{fillFormUpdate(index)}} > modifier</button>
             </div>
           <hr />
           </li>
